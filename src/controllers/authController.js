2721a330d90835a44f1b099925251adb2a984f9c
Amine Kaddouri <akaddour@e2r7p1.1337.ma>
2025-11-03 23:13:05 +0100

    Add authentication controllers (register, login, profile

---
+++ src/controllers/authController.js
@@ -0,0 +1,130 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please provide all fields');
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists with this email');
+  }
+
+  // Create user (password will be hashed automatically by pre-save middleware)
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+  if (user) {
+    res.status(200).json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        avatar: user.avatar,
+        token: generateToken(user._id)
+      }
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('Please provide email and password');
+  }
+
+  const user = await User.findOne({email}).select('+password');
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        avatar: user.avatar,
+        token: generateToken(user._id)
+      }
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password')
+  }
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        avatar: user.avatar,
+        isActive: user.isActive,
+        createdAt: user.createdAt
+      }
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+const updateUserProfile= asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      success: true,
+      data: {
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        role: updatedUser.role,
+        avatar: updatedUser.avatar,
+        token: generateToken(updatedUser._id)
+      }
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile
+};
