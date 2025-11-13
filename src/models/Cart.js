feb65204c46b242b4a3cd8fd0537d8015e361ca4
aminekaddouri <kaddouriaminee@gmail.com>
2025-11-13 23:48:23 +0100

    Add Cart model with embedded items and total calculation

---
+++ src/models/Cart.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+/**
+ * Cart Item Schema
+ * Each item in the cart
+ */
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, 'Quantity must be at least 1'],
+    default: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+/**
+ * Cart Schema
+ * One cart per user
+ */
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true, // One cart per user
+    },
+    items: [cartItemSchema],
+    totalQuantity: {
+      type: Number,
+      default: 0,
+    },
+    subtotal: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Updates totalQuantity and subtotal
+cartSchema.methods.calculateTotals = function () {
+  this.totalQuantity = this.items.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+  this.subtotal = this.item.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+};
+
+// Pre-save Middleware
+// Calculate totals before saving
+cartSchema.pre('save', function (next) {
+  this.calculateTotals();
+  next();
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
