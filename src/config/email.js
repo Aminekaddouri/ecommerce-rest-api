609eb78a52e73d1f88fa9d3dd19d2ccc75720757
aminekaddouri <kaddouriaminee@gmail.com>
2025-11-16 00:04:04 +0100

    Add email transporter and send email utility

---
+++ src/config/email.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+
+/**
+ * Create Email Transporter
+ * Uses Gmail SMTP
+ */
+const createTransporter = () => {
+  return nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+};
+
+module.exports = createTransporter;
---
+++ src/utils/sendEmail.js
@@ -0,0 +1,28 @@
+const createTransport = require('../config/email');
+
+/**
+ * Send Email
+ * @param {Object} options - Email options
+ */
+const sendEmail = async options => {
+  const transporter = createTransporter();
+
+  const mailOptions = {
+    from: `${process.env.EMAIL_FROM}`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    html: options.html || options.message,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('✅ Email sent:', info.messageId);
+    return info;
+  } catch (error) {
+    console.error('❌ Email error:', error.message);
+    throw new Error('Email could not be sent');
+  }
+};
+
+module.exports = sendEmail;
